798129833879cc031d167e4623e81d6ca75295e0
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-01-27 15:35:12 +0300

    Add ros header

---
+++ ros-native/src/ros-native/include/ros.h
@@ -0,0 +1,23 @@
+#ifndef ROS_ONE_ROS_H_
+#define ROS_ONE_ROS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "callback_queue.h"
+#include "callback.h"
+#include "common.h"
+#include "init.h"
+#include "macros.h"
+#include "master.h"
+#include "node_handle.h"
+#include "publisher.h"
+#include "spinner.h"
+#include "subscriber.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //ROS_ONE_ROS_H_
