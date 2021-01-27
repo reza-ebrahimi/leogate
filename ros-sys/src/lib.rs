bb1138f9d96c13c76de7d1b2843a509c93c3944c
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-01-27 16:56:10 +0300

    Export ros module

--- ros-sys/src/lib.rs
+++ ros-sys/src/lib.rs
@@ -1,4 +1,12 @@
-#[allow(dead_code)]
-#[allow(warnings)]
-mod ffi;
-pub use ffi::*;
+#![allow(
+    clippy::missing_safety_doc,
+    clippy::unreadable_literal,
+    dead_code,
+    non_camel_case_types,
+    non_snake_case,
+    non_upper_case_globals,
+    overflowing_literals,
+    unused_imports
+)]
+mod ros;
+pub use ros::*;
