8bee43ca57c8185ee68dd8f98f43805394276789
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-03-10 19:15:23 +0300

    Update callback

--- ros-native/src/ros-native/include/callback.h
+++ ros-native/src/ros-native/include/callback.h
@@ -5,7 +5,22 @@
 extern "C" {
 #endif
 
-typedef void (*callback)(const void *phantom_data, const char *);
+#include <stddef.h>
+
+typedef struct _binary_payload {
+  const unsigned char *payload;
+  size_t size;
+} binary_payload;
+
+typedef struct _json_payload {
+  const char *payload;
+  size_t size;
+} json_payload;
+
+typedef void (*callback)(
+    const void *phantom_data,
+    const json_payload *payload,
+    const binary_payload *bin_payload);
 
 #ifdef __cplusplus
 }
