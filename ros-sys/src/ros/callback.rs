aa63c7da11fb5b641f5223e677d19d8ed8cb5a49
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-03-15 13:56:25 +0300

    Add channel payload structure and update callback

--- ros-sys/src/ros/callback.rs
+++ ros-sys/src/ros/callback.rs
@@ -1,3 +1,12 @@
-pub trait Callback {
-    fn callback(&self, d: &String);
+use actix::prelude::*;
+
+pub trait Callback: Sync + Send {
+    fn callback(&mut self, d: &String);
+}
+
+#[derive(Debug, Clone, Message)]
+#[rtype(result = "()")]
+pub struct ChannelPayload {
+    pub json: String,
+    pub binary: Option<Vec<u8>>,
 }
