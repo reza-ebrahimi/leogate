91c2a4483f247d2582673c90991e6b94fadae525
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-03-15 13:56:55 +0300

    Regenerate FFI bindings

--- ros-sys/src/ros/ffi.rs
+++ ros-sys/src/ros/ffi.rs
@@ -297,10 +297,137 @@ extern "C" {
     #[doc = " \\brief Returns whether or not this queue is enabled"]
     pub fn callback_queue_isEnabled(cq: *mut callback_queue) -> bool;
 }
+pub type wchar_t = ::std::os::raw::c_int;
+#[repr(C)]
+#[repr(align(16))]
+#[derive(Copy, Clone)]
+pub struct max_align_t {
+    pub __clang_max_align_nonce1: ::std::os::raw::c_longlong,
+    pub __bindgen_padding_0: u64,
+    pub __clang_max_align_nonce2: u128,
+}
+#[test]
+fn bindgen_test_layout_max_align_t() {
+    assert_eq!(
+        ::std::mem::size_of::<max_align_t>(),
+        32usize,
+        concat!("Size of: ", stringify!(max_align_t))
+    );
+    assert_eq!(
+        ::std::mem::align_of::<max_align_t>(),
+        16usize,
+        concat!("Alignment of ", stringify!(max_align_t))
+    );
+    assert_eq!(
+        unsafe {
+            &(*(::std::ptr::null::<max_align_t>())).__clang_max_align_nonce1 as *const _ as usize
+        },
+        0usize,
+        concat!(
+            "Offset of field: ",
+            stringify!(max_align_t),
+            "::",
+            stringify!(__clang_max_align_nonce1)
+        )
+    );
+    assert_eq!(
+        unsafe {
+            &(*(::std::ptr::null::<max_align_t>())).__clang_max_align_nonce2 as *const _ as usize
+        },
+        16usize,
+        concat!(
+            "Offset of field: ",
+            stringify!(max_align_t),
+            "::",
+            stringify!(__clang_max_align_nonce2)
+        )
+    );
+}
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct _binary_payload {
+    pub payload: *const ::std::os::raw::c_uchar,
+    pub size: usize,
+}
+#[test]
+fn bindgen_test_layout__binary_payload() {
+    assert_eq!(
+        ::std::mem::size_of::<_binary_payload>(),
+        16usize,
+        concat!("Size of: ", stringify!(_binary_payload))
+    );
+    assert_eq!(
+        ::std::mem::align_of::<_binary_payload>(),
+        8usize,
+        concat!("Alignment of ", stringify!(_binary_payload))
+    );
+    assert_eq!(
+        unsafe { &(*(::std::ptr::null::<_binary_payload>())).payload as *const _ as usize },
+        0usize,
+        concat!(
+            "Offset of field: ",
+            stringify!(_binary_payload),
+            "::",
+            stringify!(payload)
+        )
+    );
+    assert_eq!(
+        unsafe { &(*(::std::ptr::null::<_binary_payload>())).size as *const _ as usize },
+        8usize,
+        concat!(
+            "Offset of field: ",
+            stringify!(_binary_payload),
+            "::",
+            stringify!(size)
+        )
+    );
+}
+pub type binary_payload = _binary_payload;
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct _json_payload {
+    pub payload: *const ::std::os::raw::c_char,
+    pub size: usize,
+}
+#[test]
+fn bindgen_test_layout__json_payload() {
+    assert_eq!(
+        ::std::mem::size_of::<_json_payload>(),
+        16usize,
+        concat!("Size of: ", stringify!(_json_payload))
+    );
+    assert_eq!(
+        ::std::mem::align_of::<_json_payload>(),
+        8usize,
+        concat!("Alignment of ", stringify!(_json_payload))
+    );
+    assert_eq!(
+        unsafe { &(*(::std::ptr::null::<_json_payload>())).payload as *const _ as usize },
+        0usize,
+        concat!(
+            "Offset of field: ",
+            stringify!(_json_payload),
+            "::",
+            stringify!(payload)
+        )
+    );
+    assert_eq!(
+        unsafe { &(*(::std::ptr::null::<_json_payload>())).size as *const _ as usize },
+        8usize,
+        concat!(
+            "Offset of field: ",
+            stringify!(_json_payload),
+            "::",
+            stringify!(size)
+        )
+    );
+}
+pub type json_payload = _json_payload;
 pub type callback = ::std::option::Option<
     unsafe extern "C" fn(
         phantom_data: *const ::std::os::raw::c_void,
-        arg1: *const ::std::os::raw::c_char,
+        payload: *const json_payload,
+        bin_payload: *const binary_payload,
     ),
 >;
 extern "C" {
