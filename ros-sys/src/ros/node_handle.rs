32665dc7df3918d19b78dcf53605b5fa50d9db27
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-03-15 13:59:25 +0300

    Change node_handle to accept and work with ChannelPayload struct

--- ros-sys/src/ros/node_handle.rs
+++ ros-sys/src/ros/node_handle.rs
@@ -1,28 +1,46 @@
 use std::ffi::*;
 use std::os::raw::*;
 
-use serde::{Deserialize, Serialize};
-use serde_json::{value::RawValue, Result};
-
-use super::callback::Callback;
+use super::callback::ChannelPayload;
 use super::ffi;
 use super::subscriber::Subscriber;
 
-unsafe extern "C" fn __callback_fn(phantom_data: *const c_void, msg: *const c_char) {
-    let raw_str = CStr::from_ptr(msg).to_str().unwrap();
-    let handle = phantom_data as *const NodeHandle;
+unsafe extern "C" fn __callback_fn(
+    phantom_data: *const c_void,
+    json: *const ffi::_json_payload,
+    binary: *const ffi::_binary_payload,
+) {
+    let handle = phantom_data as *const Subscriber;
 
-    (*handle).__call(&String::from(raw_str));
+    let payload = if binary.is_null() {
+        ChannelPayload {
+            json: unsafe {
+                CStr::from_ptr((*json).payload)
+                    .to_str()
+                    .unwrap()
+                    .to_string()
+            },
+            binary: None,
+        }
+    } else {
+        let binary_data = unsafe { std::slice::from_raw_parts((*binary).payload, (*binary).size) };
+
+        ChannelPayload {
+            json: "".to_string(),
+            binary: Some(binary_data.to_vec()),
+        }
+    };
+
+    (*handle).__call(payload);
 }
 
-pub struct NodeHandle<'a> {
+pub struct NodeHandle {
     node_name: String,
     __handle: *mut ffi::node_handle,
-    __callback: Option<Box<dyn Callback + 'a>>,
 }
 
-impl NodeHandle<'_> {
-    pub fn new(name: &String) -> NodeHandle {
+impl NodeHandle {
+    pub fn new(name: String) -> NodeHandle {
         unsafe {
             let ns = CString::new(name.as_str()).expect("CString::new failed");
             let nh = ffi::node_handle_create(ns.as_ptr());
@@ -30,13 +48,8 @@ impl NodeHandle<'_> {
             NodeHandle {
                 node_name: name.clone(),
                 __handle: nh,
-                __callback: None,
             }
         }
-    }
-
-    pub fn __call(&self, mgs: &String) {
-        self.__callback.as_ref().unwrap().as_ref().callback(mgs);
     }
 
     pub fn subscribe(
@@ -44,27 +57,29 @@ impl NodeHandle<'_> {
         topic: &String,
         topic_type: &String,
         queue_size: u32,
-        cb_object: Box<dyn Callback>,
-    ) -> Subscriber {
+        notifier: Box<tokio::sync::mpsc::Sender<ChannelPayload>>,
+    ) -> Box<Subscriber> {
         unsafe {
             let _topic = CString::new(topic.as_str()).expect("CString::new failed");
             let _topic_type = CString::new(topic_type.as_str()).expect("CString::new failed");
-            let _ptr: *mut c_void = self as *mut _ as *mut c_void;
 
-            self.__callback = Some(cb_object);
+            let mut subscriber = Box::new(Subscriber::new(topic, topic_type, notifier));
+            let subscriber_ptr: *const c_void = subscriber.as_ref() as *const _ as *const c_void;
 
-            let _sub: *mut ffi::subscriber = ffi::node_handle_subscribe(
+            let ffi_subscriber: *mut ffi::subscriber = ffi::node_handle_subscribe(
                 self.__handle,
                 _topic.as_ptr(),
                 _topic_type.as_ptr(),
                 queue_size,
-                _ptr,
+                subscriber_ptr,
                 Some(__callback_fn),
             );
 
-            let mut sub = Subscriber::new(topic, topic_type);
-            sub.set_handle(_sub);
-            sub
+            subscriber.set_handle(ffi_subscriber);
+            subscriber
         }
     }
 }
+
+unsafe impl Send for NodeHandle {}
+unsafe impl Sync for NodeHandle {}
