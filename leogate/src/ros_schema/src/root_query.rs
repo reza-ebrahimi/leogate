5e3e94bc6b59b343a58eef02f6b659f0b3cb7190
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-03-16 17:05:57 +0300

    [leogate crate] Add file: leogate/src/ros_schema/src/root_query.rs

---
+++ leogate/src/ros_schema/src/root_query.rs
@@ -0,0 +1,10 @@
+use super::query::*;
+use async_graphql::*;
+
+#[derive(Default, MergedObject)]
+pub struct RootQuery(
+    TopicFindQuery,
+    TopicInfoQuery,
+    TopicListQuery,
+    TopicTypeQuery,
+);
