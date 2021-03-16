eafa23bd1010d236a91b4bebb93b8b099bb315ee
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-03-16 17:05:57 +0300

    [leogate crate] Add file: leogate/src/ros_schema/src/query/topic_type.rs

---
+++ leogate/src/ros_schema/src/query/topic_type.rs
@@ -0,0 +1,11 @@
+use async_graphql::*;
+
+#[derive(Clone, Default)]
+pub struct TopicTypeQuery;
+
+#[Object]
+impl TopicTypeQuery {
+    async fn dummy(&self) -> Option<String> {
+        None
+    }
+}
