a99c5d17a438288a0b456bb11c0bf6134f3efa6a
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-03-16 17:05:57 +0300

    [leogate crate] Add file: leogate/src/ros_schema/src/root_subscription.rs

---
+++ leogate/src/ros_schema/src/root_subscription.rs
@@ -0,0 +1,10 @@
+use super::subscription::*;
+use async_graphql::*;
+
+#[derive(Default, MergedSubscription)]
+pub struct RootSubscription(
+    //TopicBandwidthSubscription,
+    //TopicDelaySubscription,
+    TopicEchoSubscription,
+    //TopicHzSubscription,
+);
