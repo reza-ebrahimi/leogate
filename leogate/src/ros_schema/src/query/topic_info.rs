de8bb03458dcaf12462c3930a1facf1af8b56da6
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-03-16 17:05:57 +0300

    [leogate crate] Add file: leogate/src/ros_schema/src/query/topic_info.rs

---
+++ leogate/src/ros_schema/src/query/topic_info.rs
@@ -0,0 +1,11 @@
+use async_graphql::*;
+
+#[derive(Clone, Default)]
+pub struct TopicInfoQuery;
+
+#[Object]
+impl TopicInfoQuery {
+    async fn dummy(&self) -> Option<String> {
+        None
+    }
+}
