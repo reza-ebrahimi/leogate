9e9be20476ef9ac5b3bcdc4114ad3368297d2080
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-03-16 17:05:56 +0300

    [leogate crate] Add file: leogate/src/ros_manager/src/ros_manager_inner.rs

---
+++ leogate/src/ros_manager/src/ros_manager_inner.rs
@@ -0,0 +1,17 @@
+use std::collections::HashMap;
+
+use ros_sys::*;
+
+pub struct RosManagerInner {
+    pub n_handle: Option<NodeHandle>,
+    pub subscribers: HashMap<String, Box<Subscriber>>,
+}
+
+impl RosManagerInner {
+    pub fn new() -> RosManagerInner {
+        RosManagerInner {
+            n_handle: None,
+            subscribers: HashMap::new(),
+        }
+    }
+}
