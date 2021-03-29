4aba3a6f265a0ff0a1188f65e8ec243c3e72786b
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-03-29 18:56:55 +0300

    Update TopicEcho subscription to new ros subscription API

--- leogate/src/ros_schema/src/subscription/topic_echo.rs
+++ leogate/src/ros_schema/src/subscription/topic_echo.rs
@@ -1,12 +1,12 @@
 use std::sync::Arc;
 
 use futures::lock::Mutex;
-use futures_util::stream::{Stream, StreamExt};
+use futures::prelude::*;
 
 use async_graphql::*;
 
-use ros_manager::RosManager;
 use ros_msgs::*;
+use ros_sys::RosManager;
 
 use crate::TopicInput;
 
@@ -24,9 +24,13 @@ impl TopicEchoSubscription {
       .data_unchecked::<Arc<Mutex<RosManager>>>()
       .lock()
       .await
-      .subscribe(input.topic, input.msg_type)
-      .await
+      .node_handle()
+      .subscribe(input.topic.as_str(), input.msg_type.as_str(), 1024)
       .map(|payload| {
+        if payload.json.is_empty() {
+          return None;
+        }
+
         let json_str = payload.json.as_str();
         let msg: Option<ros_msgs::RosMessageType> = match serde_json::from_str(json_str) {
           Ok(payload) => {
