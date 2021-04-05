17f9f973422afbc8e43e078ec0a009d60a21bef4
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-04-05 18:39:03 +0300

    Update root subscription resolvers

--- leogate/src/ros_schema/src/root_subscription.rs
+++ leogate/src/ros_schema/src/root_subscription.rs
@@ -2,9 +2,4 @@ use super::subscription::*;
 use async_graphql::*;
 
 #[derive(Default, MergedSubscription)]
-pub struct RootSubscription(
-  //TopicBandwidthSubscription,
-  //TopicDelaySubscription,
-  TopicEchoSubscription,
-  //TopicHzSubscription,
-);
+pub struct RootSubscription(TopicSubscription, TimeSubscription);
