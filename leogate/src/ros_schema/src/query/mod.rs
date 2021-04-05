fa17def62d6d1283a48f871989c18322ae417583
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-04-05 18:40:41 +0300

    Update modules

--- leogate/src/ros_schema/src/query/mod.rs
+++ leogate/src/ros_schema/src/query/mod.rs
@@ -1,9 +1,7 @@
-mod topic_find;
-mod topic_info;
-mod topic_list;
-mod topic_type;
+mod master;
+mod node;
+mod topic;
 
-pub use topic_find::TopicFindQuery;
-pub use topic_info::TopicInfoQuery;
-pub use topic_list::TopicListQuery;
-pub use topic_type::TopicTypeQuery;
+pub use master::RosMasterQuery;
+pub use node::RosNodeQuery;
+pub use topic::TopicQuery;
--- leogate/src/ros_schema/src/subscription/mod.rs
+++ leogate/src/ros_schema/src/subscription/mod.rs
@@ -1,9 +1,5 @@
-mod topic_bandwidth;
-mod topic_delay;
-mod topic_echo;
-mod topic_hz;
+mod time;
+mod topic;
 
-pub use topic_bandwidth::TopicBandwidthSubscription;
-pub use topic_delay::TopicDelaySubscription;
-pub use topic_echo::TopicEchoSubscription;
-pub use topic_hz::TopicHzSubscription;
+pub use time::TimeSubscription;
+pub use topic::TopicSubscription;
