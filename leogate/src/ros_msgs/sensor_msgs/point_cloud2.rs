717d471543eb5af233d8df2547cbd252ea8b18c1
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-04-14 13:46:12 +0300

    [ros_msgs crate] Add file: ../leogate/src/ros_msgs/sensor_msgs/

---
+++ leogate/src/ros_msgs/sensor_msgs/point_cloud2.rs
@@ -0,0 +1,17 @@
+use async_graphql::*;
+
+use super::PointField;
+use crate::ros_msgs::std_msgs::Header;
+
+#[derive(Clone, Debug, Default, SimpleObject, serde::Deserialize)]
+pub struct PointCloud2 {
+  header: Header,
+  height: u32,
+  width: u32,
+  fields: Vec<PointField>,
+  is_bigendian: bool,
+  point_step: u32,
+  row_step: u32,
+  data: Vec<u8>,
+  is_dense: bool,
+}
