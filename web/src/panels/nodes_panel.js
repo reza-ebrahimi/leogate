89a92b4a482ae40a29471ec2648a70986fb61f7a
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-04-14 13:51:53 +0300

    [web] Add file: ../web/src/panels/nodes_panel.js

---
+++ web/src/panels/nodes_panel.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+import { gql, useQuery } from "@apollo/client";
+import Network from "../network_interface";
+
+const QUERY = gql`
+  query {
+    masterInfo {
+      nodes
+    }
+  }
+`;
+
+const NodesPanelStyled = styled.div`
+  display: grid;
+  grid-template-rows: auto;
+  grid-row-gap: 10px;
+`;
+
+const NodesPanel = (props) => {
+  const { loading, error, data } = useQuery(QUERY, {
+    client: Network.ros_client,
+  });
+
+  if (error) {
+    return `Query Error! ${error.message}`;
+  }
+
+  if (loading) {
+    return null;
+  }
+
+  const nodes = data.masterInfo.nodes;
+
+  return (
+    <NodesPanelStyled>
+      {nodes.map((node, idx) => {
+        return <div key={idx}>{node}</div>;
+      })}
+    </NodesPanelStyled>
+  );
+};
+
+export default NodesPanel;
