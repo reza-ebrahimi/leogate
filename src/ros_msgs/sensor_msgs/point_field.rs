d0f3e379a0bb3ad57fdc69b859227e538995c861
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-04-14 14:22:04 +0300

    Add file to new location (src directory): src/ros_msgs/sensor_msgs/point_field.rs

---
+++ src/ros_msgs/sensor_msgs/point_field.rs
@@ -0,0 +1,9 @@
+use async_graphql::*;
+
+#[derive(Clone, Debug, Default, SimpleObject, serde::Deserialize)]
+pub struct PointField {
+  name: String,
+  offset: u32,
+  datatype: u8,
+  count: u32,
+}
