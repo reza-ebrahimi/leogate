bca8e4bfb1081fe5350cc4859292008b8d202ba2
reza-ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-04-14 13:51:52 +0300

    [web] Add file: ../web/src/layout/main.js

---
+++ web/src/layout/main.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import styled from "styled-components";
+
+import StreamViewer from "../controls/stream_viewer";
+import ControlPanel from "../controls/control_panel";
+
+const ControlPanelContainer = styled.div`
+  height: 450px;
+  background: purple;
+  overflow-y: auto;
+`;
+
+const MainContainer = styled.div`
+  display: grid;
+  grid-template-rows: 1fr auto;
+`;
+
+const MainLayout = (props) => (
+  <MainContainer>
+    {/* <StreamViewer /> */}
+    <Switch>
+      <Route path="/panels">
+        <ControlPanelContainer>
+          <ControlPanel />
+        </ControlPanelContainer>
+      </Route>
+    </Switch>
+  </MainContainer>
+);
+
+export default MainLayout;
