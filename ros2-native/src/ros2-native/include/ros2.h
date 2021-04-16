963476b1be8c0fe1de75a18a251d8ce0df43a249
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-04-16 17:12:38 +0300

    [ros2-native] add new native library: ros2-native/src/ros2-native/include/ros2.h

---
+++ ros2-native/src/ros2-native/include/ros2.h
@@ -0,0 +1,16 @@
+#ifndef ROS2_ROS2_H_
+#define ROS2_ROS2_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "callback.h"
+#include "init.h"
+#include "node.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //ROS2_ROS2_H_
