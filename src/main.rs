e1f64de0254d70f75ecd32b5be92aea313edb042
Reza Ebrahimi <reza.ebrahimi.dev@gmail.com>
2021-04-16 17:08:28 +0300

    [main] Update file: src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 mod core;
 mod ros_msgs;
 mod ros_schema;
+mod ros2_schema;
 mod sys_schema;
 
 mod main_ros;
+mod main_ros2;
 mod main_sys;
 
 fn main() -> std::io::Result<()> {
   let _ros_thread_handle = std::thread::spawn(move || {
     main_ros::main().unwrap();
+  });
+
+  let _ros2_thread_handle = std::thread::spawn(move || {
+    main_ros2::main().unwrap();
   });
 
   main_sys::main()
